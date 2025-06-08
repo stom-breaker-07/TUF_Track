8f61d328580d8c5f6557ded6ee3329deb256c048
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-06-08 22:24:01 +0530

    Spiral Matrix

---
+++ Arrays/Array15.java
@@ -0,0 +1,36 @@
+package TUF_Track.Arrays;
+
+public class Array15 {
+    class Solution {
+        public List<Integer> spiralOrder(int[][] matrix) {
+            List<Integer> ans=new ArrayList<>();
+            int total=0;
+            int r=matrix.length;
+            int c=matrix[0].length;
+            int Tr=0 ,Rc=c-1 ,Br=r-1 ,Lc=0;
+            while(total < r*c){
+                for(int j=Lc ;j<=Rc && total < r*c ;j++){
+                    ans.add(matrix[Tr][j]);
+                    total++;
+                }
+                Tr++;
+                for(int i=Tr ;i<=Br && total < r*c ;i++ ){
+                    ans.add(matrix[i][Rc]);
+                    total++;
+                }
+                Rc--;
+                for(int j=Rc;j>=Lc && total < r*c; j-- ){
+                    ans.add(matrix[Br][j]);
+                    total++;
+                }
+                Br--;
+                for(int i=Br ;i>=Tr && total < r*c;i--){
+                    ans.add(matrix[i][Lc]);
+                    total++;
+                }
+                Lc++;
+            }
+            return ans;
+        }
+    }
+}
