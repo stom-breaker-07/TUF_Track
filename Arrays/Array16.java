736be8d739323ec64e70a1eb35c466f0c46074e1
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-06-09 20:34:30 +0530

    Rotate Image

---
+++ Arrays/Array16.java
@@ -0,0 +1,34 @@
+package TUF_Track.Arrays;
+
+public class Array16 {
+    public void rotate(int[][] matrix) {
+        // get the teranspose of matrix
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i+1;j<matrix[0].length;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+
+        // swap the column
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][(matrix.length-1)-j];
+                matrix[i][(matrix.length-1)-j]=temp;
+            }
+        }
+    }
+}
+/*You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+
+
+Example 1:
+
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]*/
