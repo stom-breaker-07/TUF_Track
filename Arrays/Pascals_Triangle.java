a7ebf232dc316aaaaab03f06ae0364f03e5cf95d
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-07-19 20:47:34 +0530

    Revision

---
+++ Arrays/Pascals_Triangle.java
@@ -0,0 +1,19 @@
+package TUF_Track.Arrays;
+
+public class Pascals_Triangle {
+    public List<List<Integer>> generate(int numRows) {
+        ArrayList<List<Integer>> out= new ArrayList<>();
+        for(int i=0;i<numRows;i++){
+            ArrayList<Integer> in=new ArrayList<>();
+            in.add(1);
+            for(int j=1;j<i;j++){
+                in.add(out.get(i-1).get(j-1)+out.get(i-1).get(j));
+            }
+            if(i>0){
+                in.add(1);
+            }
+            out.add(in);
+        }
+        return out;
+    }
+}
